5b9ad3b5c665a733baa5cf87cc74f4741b78a68d
AnthonyCh05 <anthonychangotasig.clearminds@gmail.com>
2024-10-03 09:44:53 -0500

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,23 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+import { SharedModule } from './shared/shared.module';
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { GifsModule } from './gifs/gifs.module';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    SharedModule,
+    GifsModule
+  ],
+  exports: [
   ],
   providers: [],
   bootstrap: [AppComponent]
